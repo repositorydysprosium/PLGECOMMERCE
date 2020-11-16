3d43dc752a7c6394bc85f6235e67e4696f559231
Repository Desenvolvimento <repositort.desenvolvimento@gmail.com>
2020-11-15 22:12:26 -0300

    PLGECOMMERCE-09112020511719
    - Implementação da configuração da aplicação para rodar em ambiente de produção

--- src/main/java/br/com/plataformalancamento/configuration/ProfileProductionConfiguration.java
+++ src/main/java/br/com/plataformalancamento/configuration/ProfileProductionConfiguration.java
@@ -1,29 +1,15 @@
 package br.com.plataformalancamento.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import br.com.plataformalancamento.service.DatabaseService;
 
 @Configuration
 @Profile(value = "production")
 public class ProfileProductionConfiguration {
 	
-	@Autowired
-	private DatabaseService databaseService;
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategyDatabase;
-
 	@Bean
 	public boolean instantiateDatabase() {
-		if(!strategyDatabase.equals("create")) {
-			return false;
-		}
-		databaseService.instantiateTestDatabase();
 		return true;
 	}
 	
